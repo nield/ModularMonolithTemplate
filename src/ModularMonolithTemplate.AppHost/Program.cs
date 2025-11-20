5ea43de20c3b22516f990b62c0626a56bbd66d31
Niel de Wet <nieldw@globalkinetic.com>
2025-11-20 12:40:48 +0200

    chore: give aspire container names

--- src/ModularMonolithTemplate.AppHost/Program.cs
+++ src/ModularMonolithTemplate.AppHost/Program.cs
@@ -1,21 +1,27 @@
+const string baseContainerName = "modularmonolithtemplate";
+
 var builder = DistributedApplication.CreateBuilder(args);
 
 var rabbit = builder.AddRabbitMQ("RabbitMq")
-                .WithLifetime(ContainerLifetime.Persistent)
-                .WithManagementPlugin(8001);
+    .WithLifetime(ContainerLifetime.Session)
+    .WithManagementPlugin(8001)
+    .WithContainerName($"{baseContainerName}-rabbit");
 
 var seq = builder.AddSeq("Seq", 8002)
-    .WithLifetime(ContainerLifetime.Persistent);
+    .WithLifetime(ContainerLifetime.Session)
+    .WithContainerName($"{baseContainerName}-seq");
 
 var redis = builder.AddRedis("Redis", 8004)
-    .WithLifetime(ContainerLifetime.Persistent);
+    .WithLifetime(ContainerLifetime.Session)
+    .WithContainerName($"{baseContainerName}-redis");
 
 var sqlPassword = builder.AddParameter("sqlPassword");
 var database = builder.AddSqlServer("Sql", sqlPassword, 8003)
-    .WithLifetime(ContainerLifetime.Persistent)
+    .WithLifetime(ContainerLifetime.Session)
+    .WithContainerName($"{baseContainerName}-sql")
     .AddDatabase("SqlDatabase", "templateDb");
 
-builder.AddProject<Projects.ModularMonolithTemplate_Api>("modularmonolithtemplate-api")
+builder.AddProject<Projects.ModularMonolithTemplate_Api>($"{baseContainerName}-api")
     .WithReference(database)
     .WaitFor(database)
     .WithReference(redis)
